b99a7eeec2a00e4a6c896a7b629c7fe8b58e4116
scorphunter <Scorphoseninja@gmail.com>
2022-01-29 01:55:46 -0500

    some lexer work

--- src/edu/ufl/cise/plc/Lexer.java
+++ src/edu/ufl/cise/plc/Lexer.java
@@ -1,10 +1,12 @@
 package edu.ufl.cise.plc;
 
 import java.util.ArrayList;
+
+import com.sun.source.tree.Tree;
 import edu.ufl.cise.plc.IToken.Kind;
 
 /* Currently Implemented:
- * <token> : +, ;, &, !, ,, /, (, ), [, ], -, %, !=, |, ^, *
+ * <token> : +, ;, &, !, ,, /, (, ), [, ], -, %, !=, |, ^, *,=,intnum,
  */
 
 public class Lexer implements ILexer {
@@ -19,6 +21,11 @@ public class Lexer implements ILexer {
 		START,
 		HAVE_EQ,
 		HAVE_BANG,
+		HAVE_AND,
+		IN_NUM,
+		IN_FLOAT,
+		IN_INDENT
+
 		END
 	}
 	
@@ -40,15 +47,33 @@ public class Lexer implements ILexer {
 			else {
 				ch = '0';
 			}
-			startPos = position;
+			startPos = position; //maybe do this  when start happens, i feel like startPos resets after every while run
+			// will be detrimental to values that need more than one char
 			switch(state) {
 			case START -> {
 				switch(ch) {
+
 				case '+' -> {
 					tokenArr.add(new Token(Kind.PLUS, startPos, 1, String.valueOf(ch)));
 					position++;
 					break;
 				}
+				case 'a', 'A' ,'b', 'B', 'c', 'C', 'd', 'D', 'e','E' ,'f', 'F','g','G','h','H'
+					'i','I','j','J','k','K','l','L','m','M', 'n','N','o','O','p','P','q', 'Q', 'r','R','s','S','t','T'
+					'u','U','v','V', 'w','W', 'x', 'X','y','Y','z','Z','$','_'
+					->{
+					state=States.IN_INDENT;
+					position++;
+					break;
+				}
+				case '0','1','2','3','4','5','6','7','8','9'->{
+					state=States.IN_NUM;
+					position++;
+					break;
+
+				}
+
+
 				case ';' -> {
 					tokenArr.add(new Token(Kind.SEMI, startPos, 1, String.valueOf(ch)));
 					position++;
@@ -119,6 +144,11 @@ public class Lexer implements ILexer {
 					position++;
 					break;
 				}
+				case '=' -> {
+					state = States.HAVE_EQ;  // Need to determine if '!' or '!='
+					position++;
+					break;
+				}
 				case '0' -> {
 					tokenArr.add(new Token(Kind.EOF, startPos, 1, String.valueOf('h'))); // For EOF I think string doesn't matter?
 					state = States.END;
@@ -127,7 +157,82 @@ public class Lexer implements ILexer {
 				default -> throw new IllegalStateException("Lexer bug (START)");
 				}
 			}
+
+			case IN_INDENT -> {
+				switch(ch){
+					case 'a', 'A' ,'b', 'B', 'c', 'C', 'd', 'D', 'e','E' ,'f', 'F','g','G','h','H'
+						'i','I','j','J','k','K','l','L','m','M', 'n','N','o','O','p','P','q', 'Q', 'r','R','s','S','t','T'
+						'u','U','v','V', 'w','W', 'x', 'X','y','Y','z','Z','$','_','0','1','2','3','4','5','6','7','8','9'
+						,'+', '*','='
+						->{
+						position++;
+						break;
+
+					}
+					default ->{
+						token.add(new Token(Kind.IN_IDENT),startPos, position-startPos, String.valueOf(inputString.substring(startPos, position))));
+						state=state.START;												//check if right
+					}
+
+				}
+
+
+			}
+			case IN_NUM -> {
+				switch(ch){
+					case '0','1','2','3','4','5','6','7','8','9'->{
+
+						position++;
+					}
+					case '.'{
+						position++;
+						state = States.IN_FLOAT;
+
+					}
+					default ->{
+						token.add(new Token(Kind.IN_IDENT),startPos, position-startPos, String.valueOf(inputString.substring(startPos, position))));
+						state=state.START;												//check if right
+					}
+
+				}
+
+			}
+				case IN_FLOAT -> {
+					case '0','1','2','3','4','5','6','7','8','9'->{
+						//still need to consider cases with leading 0s at end of decimal
+						position++;
+					}
+					default ->{
+						token.add(new Token(Kind.IN_FLOAT),startPos, position-startPos, String.valueOf(inputString.substring(startPos, position))));
+						state=state.START;
+					}
+
+				}
+
 			case HAVE_EQ -> {
+				//Determine if ==
+				if(inputString.length() > position) {
+					ch = inputString.charAt(position);
+				}
+				else{
+					tokenArr.add(new Token(Kind.ASSIGN, startPos, 1, String.valueOf(ch)));
+					state = States.START;
+					break;
+				}
+				if(ch == '=') {
+					tokenArr.add(new Token(Kind.EQUALS, startPos, 2, inputString.substring(startPos, position)));
+					position++;
+					state = States.START;
+				}
+				elseif( ch!= '='){
+					tokenARR.add(new Token(Kind.ASSIGN,startPos, 1,String.valueOF(ch)));
+					//Dont Increment Position
+				}
+				else{
+					throw new IllegalStateException("Lexer bug (HAVE_EQ)");
+				}
+
+
 				
 			}
 			// Need to determine if '!' or '!='
@@ -143,7 +248,8 @@ public class Lexer implements ILexer {
 					break;
 				}
 				if(ch == '=') {
-					tokenArr.add(new Token(Kind.NOT_EQUALS, startPos, 2, String.valueOf(ch)));
+					tokenArr.add(new Token(Kind.NOT_EQUALS, startPos, 2, inputString.substring(startPos, position)));
+					//check if input positioning is right
 					position++;
 					state = States.START;
 				}
@@ -156,6 +262,9 @@ public class Lexer implements ILexer {
 					throw new IllegalStateException("Lexer bug (HAVE_BANG)");
 				}
 			}
+
+
+
 			default -> throw new IllegalStateException("Lexer bug");
 			}
 			if(state == States.END) {
@@ -174,6 +283,7 @@ public class Lexer implements ILexer {
 			throw new LexicalException("Attempted to access out of bounds.");
 		}
 	}
+
 
 	@Override
 	public IToken peek() throws LexicalException {
