1f3d0165521300a555a0bb742f47fcc78dac3684
scorphunter <Scorphoseninja@gmail.com>
2022-01-31 15:14:52 -0500

    fixed some bugs.

--- src/edu/ufl/cise/plc/Lexer.java
+++ src/edu/ufl/cise/plc/Lexer.java
@@ -1,5 +1,5 @@
 package edu.ufl.cise.plc;
-
+import java.util.*;
 import java.util.ArrayList;
 
 import com.sun.source.tree.Tree;
@@ -10,43 +10,46 @@ import edu.ufl.cise.plc.IToken.Kind;
  */
 
 public class Lexer implements ILexer {
-	
+	HashMap<String, Kind> reservedWords;
 	private String inputString;
 	private int position = 0;
 	private States state;
 	private ArrayList<IToken> tokenArr = new ArrayList<IToken>();
 	private int arrListIndex = 0;
-//make prettier map if needed
-	private Map<string, Kind> reservedWords;
-	reservedWords.put("BLACK", Kind.COLOR_CONST);
-	reservedWords.put("BLUE",  Kind.COLOR_CONST);
-	reservedWords.put("CYAN",  Kind.COLOR_CONST);
-	reservedWords.put("DARK_GRAY", Kind.COLOR_CONST);
-	reservedWords.put("GRAY", Kind.COLOR_CONST);
-	reservedWords.put("GREEN", Kind.COLOR_CONST);
-	reservedWords.put("LIGHT_GRAY", Kind.COLOR_CONST);
-	reservedWords.put("MAGENTA", Kind.COLOR_CONST);
-	reservedWords.put("ORANGE", Kind.COLOR_CONST);
-	reservedWords.put("PINK", Kind.COLOR_CONST);
-	reservedWords.put("RED", Kind.COLOR_CONST);
-	reservedWords.put("WHITE", Kind.COLOR_CONST);
-	reservedWords.put("YELLOW", Kind.COLOR_CONST);
-	reservedWords.put("true", Kind.BOOLEAN_LIT);
-	reservedWords.put("false", Kind.BOOLEAN_LIT);
+	//make prettier map if needed
+	public Lexer(){
+		reservedWords= new HashMap<String, Kind>();;
+		reservedWords.put("BLACK", Kind.COLOR_CONST);
+		reservedWords.put("BLUE",  Kind.COLOR_CONST);
+		reservedWords.put("CYAN",  Kind.COLOR_CONST);
+		reservedWords.put("DARK_GRAY", Kind.COLOR_CONST);
+		reservedWords.put("GRAY", Kind.COLOR_CONST);
+		reservedWords.put("GREEN", Kind.COLOR_CONST);
+		reservedWords.put("LIGHT_GRAY", Kind.COLOR_CONST);
+		reservedWords.put("MAGENTA", Kind.COLOR_CONST);
+		reservedWords.put("ORANGE", Kind.COLOR_CONST);
+		reservedWords.put("PINK", Kind.COLOR_CONST);
+		reservedWords.put("RED", Kind.COLOR_CONST);
+		reservedWords.put("WHITE", Kind.COLOR_CONST);
+		reservedWords.put("YELLOW", Kind.COLOR_CONST);
+		reservedWords.put("true", Kind.BOOLEAN_LIT);
+		reservedWords.put("false", Kind.BOOLEAN_LIT);
 
-	reservedWords.put("if", Kind.KW_IF);
-	reservedWords.put("fi", Kind.KW_FI);
-	reservedWords.put("else", Kind.KW_ELSE);
-	reservedWords.put("write", Kind.KW_WRITE);
-	reservedWords.put("console", Kind.KW_CONSOLE);
-	reservedWords.put("getWidth", Kind.IMAGE_OP);
-	reservedWords.put("getHeight", Kind.IMAGE_OP);
-	reservedWords.put("int", Kind.TYPE);
-	reservedWords.put("float", Kind.TYPE);
-	reservedWords.put("string", Kind.TYPE);
-	reservedWords.put("boolean", Kind.TYPE);
-	reservedWords.put("color", Kind.TYPE);
-	reservedWords.put("image", Kind.TYPE);
+		reservedWords.put("if", Kind.KW_IF);
+		reservedWords.put("fi", Kind.KW_FI);
+		reservedWords.put("else", Kind.KW_ELSE);
+		reservedWords.put("write", Kind.KW_WRITE);
+		reservedWords.put("console", Kind.KW_CONSOLE);
+		reservedWords.put("getWidth", Kind.IMAGE_OP);
+		reservedWords.put("getHeight", Kind.IMAGE_OP);
+		reservedWords.put("int", Kind.TYPE);
+		reservedWords.put("float", Kind.TYPE);
+		reservedWords.put("string", Kind.TYPE);
+		reservedWords.put("boolean", Kind.TYPE);
+		reservedWords.put("color", Kind.TYPE);
+		reservedWords.put("image", Kind.TYPE);
+	}
+
 
 	//DFA doesnt mention color_op on reserved? Add?
 
@@ -57,15 +60,15 @@ public class Lexer implements ILexer {
 		HAVE_AND,
 		IN_NUM,
 		IN_FLOAT,
-		IN_INDENT
+		IN_INDENT,
 
 		END
 	}
-	
+
 	public Lexer(String input) {
 		this.inputString = input;
 	}
-	
+
 	// Note that returning interface allows a function to return anything that implements
 	// that interface.
 	@Override
@@ -84,243 +87,241 @@ public class Lexer implements ILexer {
 			startPos = position; //maybe do this  when start happens, i feel like startPos resets after every while run
 			// will be detrimental to values that need more than one char. Maybe, update startpos to position to the start case, tell if wrong;
 			switch(state) {
-			case START -> {
-				switch(ch) {
+				case START -> {
+					switch(ch) {
 
-				case '+' -> {
-					tokenArr.add(new Token(Kind.PLUS, startPos, 1, String.valueOf(ch)));
-					position++;
-					break;
-				}
-				case 'a', 'A' ,'b', 'B', 'c', 'C', 'd', 'D', 'e','E' ,'f', 'F','g','G','h','H'
-					'i','I','j','J','k','K','l','L','m','M', 'n','N','o','O','p','P','q', 'Q', 'r','R','s','S','t','T'
-					'u','U','v','V', 'w','W', 'x', 'X','y','Y','z','Z','$','_'
-					->{
-					state=States.IN_INDENT;
-					position++;
-					break;
-				}
-				case '0','1','2','3','4','5','6','7','8','9'->{
-					state=States.IN_NUM;
-					position++;
-					break;
+						case '+' -> {
+							tokenArr.add(new Token(Kind.PLUS, startPos, 1, String.valueOf(ch)));
+							position++;
+							break;
+						}
+						case 'a', 'A' ,'b', 'B', 'c', 'C', 'd', 'D', 'e','E' ,'f', 'F','g','G','h','H',
+								'i','I','j','J','k','K','l','L','m','M', 'n','N','o','O','p','P','q', 'Q', 'r','R','s','S','t','T',
+								'u','U','v','V', 'w','W', 'x', 'X','y','Y','z','Z','$','_'-> {
+							state=States.IN_INDENT;
+							position++;
+							break;
+						}
+						case '1','2','3','4','5','6','7','8','9'->{
+							state=States.IN_NUM;
+							position++;
+							break;
 
-				}
-
-
-				case ';' -> {
-					tokenArr.add(new Token(Kind.SEMI, startPos, 1, String.valueOf(ch)));
-					position++;
-					break;
-				}
-				case '|' -> {
-					tokenArr.add(new Token(Kind.OR, startPos, 1, String.valueOf(ch)));
-					position++;
-					break;
-				}
-				case '%' -> {
-					tokenArr.add(new Token(Kind.MOD, startPos, 1, String.valueOf(ch)));
-					position++;
-					break;
-				}
-				case '^' -> {
-					tokenArr.add(new Token(Kind.RETURN, startPos, 1, String.valueOf(ch)));
-					position++;
-					break;
-				}
-				case '&' -> {
-					tokenArr.add(new Token(Kind.AND, startPos, 1, String.valueOf(ch)));
-					position++;
-					break;
-				}
-				case ',' -> {
-					tokenArr.add(new Token(Kind.COMMA, startPos, 1, String.valueOf(ch)));
-					position++;
-					break;
-				}
-				case '/' -> {
-					tokenArr.add(new Token(Kind.DIV, startPos, 1, String.valueOf(ch)));
-					position++;
-					break;
-				}
-				case '(' -> {
-					tokenArr.add(new Token(Kind.LPAREN, startPos, 1, String.valueOf(ch)));
-					position++;
-					break;
-				}
-				case ')' -> {
-					tokenArr.add(new Token(Kind.RPAREN, startPos, 1, String.valueOf(ch)));
-					position++;
-					break;
-				}
-				case '[' -> {
-					tokenArr.add(new Token(Kind.LSQUARE, startPos, 1, String.valueOf(ch)));
-					position++;
-					break;
-				}
-				case ']' -> {
-					tokenArr.add(new Token(Kind.RSQUARE, startPos, 1, String.valueOf(ch)));
-					position++;
-					break;
-				}
-				case '-' -> {
-					tokenArr.add(new Token(Kind.MINUS, startPos, 1, String.valueOf(ch)));
-					position++;
-					break;
-				}
-				case '*' -> {
-					tokenArr.add(new Token(Kind.TIMES, startPos, 1, String.valueOf(ch)));
-					position++;
-					break;
-				}
-				case '!' -> {
-					state = States.HAVE_BANG;  // Need to determine if '!' or '!='
-					position++;
-					break;
-
-				}
-				case '=' -> {
-					state = States.HAVE_EQ;  // Need to determine if '!' or '!='
-					position++;
-					break;
-				}
-				case position > inputString.length() -> {
-					//check if equation is right
-					tokenArr.add(new Token(Kind.EOF, startPos, 1, String.valueOf('h'))); // For EOF I think string doesn't matter?
-					state = States.END;
-					break;
-				}
-				default -> throw new IllegalStateException("Lexer bug (START)");
-				}
-			}
-
-			case IN_INDENT -> {
-				//not sure if this will be STRING_LIT or Ident, check with teacher
-				switch(ch){
-					case 'a', 'A' ,'b', 'B', 'c', 'C', 'd', 'D', 'e','E' ,'f', 'F','g','G','h','H'
-						'i','I','j','J','k','K','l','L','m','M', 'n','N','o','O','p','P','q', 'Q', 'r','R','s','S','t','T'
-						'u','U','v','V', 'w','W', 'x', 'X','y','Y','z','Z','$','_','0','1','2','3','4','5','6','7','8','9'
-						//,'+', '*','=' 			DFA says these values not needed?
-						->{
-						position++;
-						break;
-
-					}
-					//need to set things like forbidden words  and true and false here
-
-					default ->{
-
-						String value = inputString.substring(startPos,position));
-						if(reservedWords.containsKey(value){
-							token.add(new Token(reservedWords.get(value, startPos, position-startPos,String.valueOf(value ));
 						}
 
 
-						else{
+						case ';' -> {
+							tokenArr.add(new Token(Kind.SEMI, startPos, 1, String.valueOf(ch)));
+							position++;
+							break;
+						}
+						case '|' -> {
+							tokenArr.add(new Token(Kind.OR, startPos, 1, String.valueOf(ch)));
+							position++;
+							break;
+						}
+						case '%' -> {
+							tokenArr.add(new Token(Kind.MOD, startPos, 1, String.valueOf(ch)));
+							position++;
+							break;
+						}
+						case '^' -> {
+							tokenArr.add(new Token(Kind.RETURN, startPos, 1, String.valueOf(ch)));
+							position++;
+							break;
+						}
+						case '&' -> {
+							tokenArr.add(new Token(Kind.AND, startPos, 1, String.valueOf(ch)));
+							position++;
+							break;
+						}
+						case ',' -> {
+							tokenArr.add(new Token(Kind.COMMA, startPos, 1, String.valueOf(ch)));
+							position++;
+							break;
+						}
+						case '/' -> {
+							tokenArr.add(new Token(Kind.DIV, startPos, 1, String.valueOf(ch)));
+							position++;
+							break;
+						}
+						case '(' -> {
+							tokenArr.add(new Token(Kind.LPAREN, startPos, 1, String.valueOf(ch)));
+							position++;
+							break;
+						}
+						case ')' -> {
+							tokenArr.add(new Token(Kind.RPAREN, startPos, 1, String.valueOf(ch)));
+							position++;
+							break;
+						}
+						case '[' -> {
+							tokenArr.add(new Token(Kind.LSQUARE, startPos, 1, String.valueOf(ch)));
+							position++;
+							break;
+						}
+						case ']' -> {
+							tokenArr.add(new Token(Kind.RSQUARE, startPos, 1, String.valueOf(ch)));
+							position++;
+							break;
+						}
+						case '-' -> {
+							tokenArr.add(new Token(Kind.MINUS, startPos, 1, String.valueOf(ch)));
+							position++;
+							break;
+						}
+						case '*' -> {
+							tokenArr.add(new Token(Kind.TIMES, startPos, 1, String.valueOf(ch)));
+							position++;
+							break;
+						}
+						case '!' -> {
+							state = States.HAVE_BANG;  // Need to determine if '!' or '!='
+							position++;
+							break;
 
-						token.add(new Token(Kind.IN_IDENT,startPos, position-startPos, String.valueOf(value)));
-						state=state.START;												//check if right
+						}
+						case '=' -> {
+							state = States.HAVE_EQ;  // Need to determine if '!' or '!='
+							position++;
+							break;
+						}
+						case '0' -> {
+							//this needs to be fixed
+							tokenArr.add(new Token(Kind.EOF, startPos, 1, String.valueOf('h'))); // For EOF I think string doesn't matter?
+							state = States.END;
+							break;
+						}
+						default -> throw new IllegalStateException("Lexer bug (START)");
+					}
+				}
+
+				case IN_INDENT -> {
+					//not sure if this will be STRING_LIT or Ident, check with teacher
+					switch (ch) {
+						case 'a', 'A', 'b', 'B', 'c', 'C', 'd', 'D', 'e', 'E', 'f', 'F', 'g', 'G', 'h', 'H',
+								'i', 'I', 'j', 'J', 'k', 'K', 'l', 'L', 'm', 'M', 'n', 'N', 'o', 'O', 'p', 'P', 'q', 'Q', 'r', 'R', 's', 'S', 't', 'T',
+								'u', 'U', 'v', 'V', 'w', 'W', 'x', 'X', 'y', 'Y', 'z', 'Z', '$', '_', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+								//,'+', '*','=' 			DFA says these values not needed?
+								-> {
+							position++;
+							break;
+
+						}
+						//need to set things like forbidden words  and true and false here
+
+						default -> {
+
+							String value = inputString.substring(startPos, position);
+							if (reservedWords.containsKey(value)) {
+								tokenArr.add(new Token(reservedWords.get(value), startPos, position - startPos, String.valueOf(value)));
+							} else {
+
+								tokenArr.add(new Token(Kind.IDENT, startPos, position - startPos, String.valueOf(value)));
+								state = state.START;                                                //check if right
+							}
+
+						}
+
+
+					}
+				}
+				case IN_NUM -> {
+					switch(ch){
+						case '0','1','2','3','4','5','6','7','8','9'->{
+
+							position++;
+						}
+						case '.'->{
+							position++;
+							state = States.IN_FLOAT;
+
+						}
+						default ->{
+							tokenArr.add(new Token(Kind.IDENT,startPos, position-startPos, String.valueOf(inputString.substring(startPos, position))));
+							state=state.START;												//check if right
+						}
+
 					}
 
 				}
-
-
-			}
-			case IN_NUM -> {
-				switch(ch){
-					case '0','1','2','3','4','5','6','7','8','9'->{
-
-						position++;
-					}
-					case '.'{
-						position++;
-						state = States.IN_FLOAT;
-
-					}
-					default ->{
-						token.add(new Token(Kind.IN_IDENT,startPos, position-startPos, String.valueOf(inputString.substring(startPos, position))));
-						state=state.START;												//check if right
-					}
-
-				}
-
-			}
 				case IN_FLOAT -> {
-					case '0','1','2','3','4','5','6','7','8','9'->{
-						//still need to consider cases with leading 0s at end of decimal
+					switch (ch) {
+						case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' -> {
+							//still need to consider cases with leading 0s at end of decimal
+							position++;
+						}
+						default -> {
+							tokenArr.add(new Token(Kind.FLOAT_LIT, startPos, position - startPos, String.valueOf(inputString.substring(startPos, position))));
+							state = state.START;
+						}
+
+					}
+				}
+				case HAVE_EQ -> {
+					//Determine if ==
+					if(inputString.length() > position) {
+						ch = inputString.charAt(position);
+					}
+					else{
+						tokenArr.add(new Token(Kind.ASSIGN, startPos, 1, String.valueOf(ch)));
+						state = States.START;
+						break;
+					}
+					if(ch == '=') {
+						tokenArr.add(new Token(Kind.EQUALS, startPos, 2, inputString.substring(startPos, position)));
 						position++;
+						state = States.START;
 					}
-					default ->{
-						token.add(new Token(Kind.IN_FLOAT,startPos, position-startPos, String.valueOf(inputString.substring(startPos, position))));
-						state=state.START;
+					else if( ch!= '='){
+						tokenArr.add(new Token(Kind.ASSIGN,startPos, 1,String.valueOf(ch)));
+						//Dont Increment Position
+					}
+					else{
+						throw new IllegalStateException("Lexer bug (HAVE_EQ)");
 					}
 
-				}
-
-			case HAVE_EQ -> {
-				//Determine if ==
-				if(inputString.length() > position) {
-					ch = inputString.charAt(position);
-				}
-				else{
-					tokenArr.add(new Token(Kind.ASSIGN, startPos, 1, String.valueOf(ch)));
-					state = States.START;
-					break;
-				}
-				if(ch == '=') {
-					tokenArr.add(new Token(Kind.EQUALS, startPos, 2, inputString.substring(startPos, position)));
-					position++;
-					state = States.START;
-				}
-				elseif( ch!= '='){
-					tokenARR.add(new Token(Kind.ASSIGN,startPos, 1,String.valueOF(ch)));
-					//Dont Increment Position
-				}
-				else{
-					throw new IllegalStateException("Lexer bug (HAVE_EQ)");
-				}
 
 
-				
-			}
-			// Need to determine if '!' or '!='
-			case HAVE_BANG -> {
-				// Check for end of file
-				if(inputString.length() > position) {
-					ch = inputString.charAt(position);  // get current character
 				}
-				// This would be a string like "HI!"
-				else {
-					tokenArr.add(new Token(Kind.BANG, startPos, 1, String.valueOf(ch)));
-					state = States.START;
-					break;
+				// Need to determine if '!' or '!='
+				case HAVE_BANG -> {
+					// Check for end of file
+					if(inputString.length() > position) {
+						ch = inputString.charAt(position);  // get current character
+					}
+					// This would be a string like "HI!"
+					else {
+						tokenArr.add(new Token(Kind.BANG, startPos, 1, String.valueOf(ch)));
+						state = States.START;
+						break;
+					}
+					if(ch == '=') {
+						tokenArr.add(new Token(Kind.NOT_EQUALS, startPos, 2, inputString.substring(startPos, position)));
+						//check if input positioning is right
+						position++;
+						state = States.START;
+					}
+					else if(ch != '=') {
+						tokenArr.add(new Token(Kind.BANG, startPos, 1, String.valueOf(ch)));
+						state = States.START;
+						// DONT INCREMENT POSITION HERE!
+					}
+					else {
+						throw new IllegalStateException("Lexer bug (HAVE_BANG)");
+					}
 				}
-				if(ch == '=') {
-					tokenArr.add(new Token(Kind.NOT_EQUALS, startPos, 2, inputString.substring(startPos, position)));
-					//check if input positioning is right
-					position++;
-					state = States.START;
-				}
-				else if(ch != '=') {
-					tokenArr.add(new Token(Kind.BANG, startPos, 1, String.valueOf(ch)));
-					state = States.START;
-					// DONT INCREMENT POSITION HERE!
-				}
-				else {
-					throw new IllegalStateException("Lexer bug (HAVE_BANG)");
-				}
-			}
 
 
 
-			default -> throw new IllegalStateException("Lexer bug");
+				default -> throw new IllegalStateException("Lexer bug");
 			}
 			if(state == States.END) {
 				break;
 			}
-			
+
 		}
-		
+
 		// Find return token
 		if(arrListIndex < tokenArr.size()) {
 			IToken returnToken = tokenArr.get(arrListIndex);
