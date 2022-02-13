611082b1ddd3da290bdf68ba6fd934d91d85c972
scorphunter <Scorphoseninja@gmail.com>
2022-02-13 03:52:39 -0500

    parser

---
+++ src/edu/ufl/cise/plc/Parser.java
@@ -0,0 +1,79 @@
+package edu.ufl.cise.plc;
+
+import edu.ufl.cise.plc.ast.ASTNode;
+import java.util.*;
+
+public class Parser implements IParser {
+    private final List<Token> tokens;
+    int curr = 0;
+    ASTNode parse() throws PLCException{
+
+        //change if needed
+        Parser(List<Token> tokens) {
+            this.tokens = tokens;
+        }
+
+
+
+    }
+    void EXP(IToken token){
+
+        if()
+
+
+    }
+    void ConditionalExpr(){
+
+    }
+    void LogicalOrExpr(){
+
+    }
+
+    void LogicalAndExpr(){
+
+    }
+    void AdditiveExpr (){
+
+    }
+    void MultiplicativeExpr(){
+
+    }
+    void UnaryExpr(){
+
+    }
+    void UnaryExprPostfix(){
+
+    }
+    void PrimaryExpr(){
+
+    }
+    void PixelSelector(){
+
+    }
+    String First(){
+
+    }
+    String Forward(){
+
+    }
+    String Predict(){
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
