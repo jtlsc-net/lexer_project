ef57361e4205d3ad71820ef66f22a143db5839af
scorphunter <Scorphoseninja@gmail.com>
2022-02-16 14:40:10 -0500

    new

--- src/edu/ufl/cise/plc/Parser.java
+++ src/edu/ufl/cise/plc/Parser.java
@@ -5,7 +5,7 @@ import edu.ufl.cise.plc.ast.*;
 import jdk.jshell.Snippet;
 
 import java.util.*;
-//TODO make parse function, make the error functions,
+//TODO make parse function, make the error functions, check for errors for the functions that don't check for them yet
 public class Parser implements IParser {
     private final ArrayList<Token> tokens;
     int curr = 0;
@@ -20,24 +20,45 @@ public class Parser implements IParser {
         Expr ast = expr();
 
 
+        return ast;
+
     }
+
+    //TODO, improve code namely maybe have predict sets? Either way, just incorporate errors
     Expr expr(){
+        IToken firstToken = t;
+        Expr e  = null;
+        if(isKind(IToken.Kind.KW_IF)){
+            e = ConditionalExpr()
+        }
+        else{
+            e = LogicalAndExpr();
 
-        if()
+        }
 
 
+        return e;
     }
 
     //TODO expr not done
     public Expr ConditionalExpr(){
         IToken firstToken = t;
         Expr e  = null;
-        match(IToken.Kind.KW_IF);
-        match(IToken.Kind.LPAREN);
-        expr()
-
-
-
+        Expr e2 = null;
+        Expr e3 = null;
+        if(isKind(IToken.Kind.KW_IF)) {
+            match(IToken.Kind.KW_IF);
+            match(IToken.Kind.LPAREN);
+            e = expr();
+            match(IToken.Kind.RPAREN);
+            e2 = expr();
+            match(IToken.Kind.KW_ELSE);
+            e3 = expr();
+            match(IToken.Kind.KW_FI);
+            e = new ConditionalExpr(firstToken, e, e2, e3);
+        }else{
+            throw error();
+        }
         return e;
     }
     //TODO
@@ -74,6 +95,37 @@ public class Parser implements IParser {
         return left;
     }
     Expr ComparisonExpr (){
+        IToken firstToken = t;
+        Expr left = null;
+        Expr right = null;
+        left= AdditiveExpr();
+        while (isKind(IToken.Kind.GE, IToken.Kind.LE, IToken.Kind.EQUALS, IToken.Kind.NOT_EQUALS, IToken.Kind.GT, IToken.Kind.LT)){
+            IToken op = t;
+            if(isKind(IToken.Kind.GE)) {
+                consume(IToken.Kind.GE,"");    //what to put in message?
+            }
+            if(isKind(IToken.Kind.EQUALS)) {
+                consume(IToken.Kind.EQUALS,"");    //what to put in message?
+            }
+            if(isKind(IToken.Kind.NOT_EQUALS)) {
+                consume(IToken.Kind.NOT_EQUALS,"");    //what to put in message?
+            }
+            if(isKind(IToken.Kind.LT)) {
+                consume(IToken.Kind.LT,"");    //what to put in message?
+            }
+            if(isKind(IToken.Kind.GT)) {
+                consume(IToken.Kind.GT,"");    //what to put in message?
+            }
+
+            else{//else if or else
+                consume(IToken.Kind.LE,"");
+            }
+            right= AdditiveExpr ();
+            left = new BinaryExpr(firstToken,left,op,right);
+        }
+        return left;
+
+
 
     }
     Expr AdditiveExpr (){
@@ -98,29 +150,65 @@ public class Parser implements IParser {
 
     //TODO
     Expr MultiplicativeExpr(){
+        IToken firstToken = t;
+        Expr left = null;
+        Expr right = null;
         UnaryExpr();
         while (isKind(IToken.Kind.TIMES, IToken.Kind.DIV)){
+            IToken op = t;
             if(isKind(IToken.Kind.TIMES)) {
                 consume(IToken.Kind.TIMES,"");    //what to put in message?
             }
             else{//else if or else
                 consume(IToken.Kind.DIV,"");
             }
-            UnaryExpr();
+            right = UnaryExpr();
+            left = new BinaryExpr(firstToken,left,op,right);
+
         }
-        return;
+        return left;
     }
-    void UnaryExpr(){
+    Expr UnaryExpr(){
+        IToken firstToken = t;
+        Expr e = null;
+        Expr e1= null;
+
+        if(isKind(IToken.Kind.BANG, IToken.Kind.MINUS, IToken.Kind.COLOR_CONST, IToken.Kind.IMAGE_OP)){
+            IToken op = t;
+            if(isKind(IToken.Kind.COLOR_CONST)) {
+                consume(IToken.Kind.COLOR_CONST,"");    //what to put in message?
+            }
+            if(isKind(IToken.Kind.IMAGE_OP)) {
+                consume(IToken.Kind.IMAGE_OP,"");    //what to put in message?
+            }
+            if(isKind(IToken.Kind.BANG)) {
+                consume(IToken.Kind.BANG,"");    //what to put in message?
+            }
+            else{//else if or else
+                consume(IToken.Kind.MINUS,"");
+            }
+            e = UnaryExpr();
+            e1 = new UnaryExpr(firstToken, op,  e);
+        }
+        else if(){
+
+        }
+
+
+
+
 
     }
-    void UnaryExprPostfix(){
+    Expr UnaryExprPostfix(){
+        IToken firstToken = t;
+        Expr e = null;
 
+        e = PrimaryExpr();
+
+
+        //TODO   figure out ? 0 or 1 code
     }
-    void PrimaryExpr(){
-
-    }
-    void PixelSelector(){
-
+    Expr PrimaryExpr(){
         IToken firstToken = t;
         Expr e = null;
 
@@ -153,11 +241,31 @@ public class Parser implements IParser {
         else{
             throw error();
         }
-        return;
+        return e;
+
+    }
+    Expr PixelSelector(){
+
+        IToken firstToken = t;
+        Expr left = null;
+        Expr right = null;
+        if(isKind(IToken.Kind.LSQUARE)){
+            consume(IToken.Kind.LSQUARE, "Expected left param");
+            left = expr();
+            match();
+            right = expr();
+            consume(IToken.Kind.RSQUARE, "Expected right param");
+            //TODO, this function returns an AST, not sure what to return
+            ASTNode ast = new PixelSelector(firstToken, left, right);
+        }
+        else{
+            throw error();
+        }
+
     }
 
 
-}
+
 
     //basically copy pasted from the book, which i think is allowed, but check later if this type of stuff needs
 //changed
@@ -189,12 +297,14 @@ public class Parser implements IParser {
     private Token previous() {
         return tokens.get(curr - 1);
     }
+
+    // TODO check consume and match if which one we need to use
     private Token consume(IToken.Kind kind, String message) {
         if (check(kind)) return advance();
 
         throw error(peek(), message);
     }
-
+    //TODO change this
     private ParseError error(Token token, String message) {
         Lox.error(token, message);
         return new ParseError();
