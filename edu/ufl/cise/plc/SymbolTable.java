455bd8e5dfaacedcf9db78b461397074bd037a30
scorphunter <Scorphoseninja@gmail.com>
2022-03-21 00:46:58 -0400

    preliminary

---
+++ edu/ufl/cise/plc/SymbolTable.java
@@ -0,0 +1,22 @@
+package edu.ufl.cise.plc;
+
+import edu.ufl.cise.plc.ast.Declaration;
+import edu.ufl.cise.plc.ast.Types;
+
+import java.util.HashMap;
+
+public class SymbolTable {
+
+//TODO:  Implement a symbol table class that is appropriate for this language. 
+HashMap<String, Declaration> symbolMap = new HashMap<>();
+
+    public boolean insert(String name, Declaration declaration) {
+        return (symbolMap.putIfAbsent(name,declaration) == null);
+    }
+
+public Declaration  lookup(String name){
+    return symbolMap.get(name);
+}
+
+
+}
--- edu/ufl/cise/plc/ast/AssignmentStatement.java
+++ edu/ufl/cise/plc/ast/AssignmentStatement.java
@@ -1,13 +1,14 @@
 package edu.ufl.cise.plc.ast;
 
 import edu.ufl.cise.plc.IToken;
-import edu.ufl.cise.plc.ast.Types.Type;
 
 public class AssignmentStatement extends Statement {
 
 	final String name;
 	final PixelSelector selector;
 	final Expr expr;
+//	Type targetType;
+	Declaration targetDec;
 
 	
 	public AssignmentStatement(IToken firstToken, String name, PixelSelector selector, Expr expr) {
@@ -29,6 +30,24 @@ public class AssignmentStatement extends Statement {
 		return expr;
 	}
 	
+	
+	
+//	public Type getTargetType() {
+//		return targetType;
+//	}
+
+//	public void setTargetType(Type targetType) {
+//		this.targetType = targetType;
+//	}
+
+	public Declaration getTargetDec() {
+		return targetDec;
+	}
+
+	public void setTargetDec(Declaration targetDec) {
+		this.targetDec = targetDec;
+	}
+
 	@Override
 	public Object visit(ASTVisitor v, Object arg) throws Exception {
 		return v.visitAssignmentStatement(this, arg);
