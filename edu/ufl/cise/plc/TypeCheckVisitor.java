7b03316ef054adeffc5a237074840dca05455c61
jtlsc-net <jlumshuechan@ufl.edu>
2022-03-24 23:16:50 -0400

    added vardec

--- edu/ufl/cise/plc/TypeCheckVisitor.java
+++ edu/ufl/cise/plc/TypeCheckVisitor.java
@@ -452,7 +452,20 @@ public class TypeCheckVisitor implements ASTVisitor {
 	public Object visitVarDeclaration(VarDeclaration declaration, Object arg) throws Exception {
 		//TODO:  implement this method
 
-		throw new UnsupportedOperationException("Unimplemented visit method.");
+		// throw new UnsupportedOperationException("Unimplemented visit method.");
+		
+		String name = declaration.getName();
+		boolean inserted = symbolTable.insert(name,declaration);
+		check(inserted, declaration, "variable " + name + "already declared");
+		Expr initializer = declaration.getExpr();
+		if (initializer != null) {
+			//infer type of initializer
+			Type initializerType = (Type) initializer.getType();  // Get type of Expr on rhs of declaration.
+			check(assignmentCompatible(declaration.getType(), initializerType),declaration, 
+			"type of expression and declared type do not match");
+			declaration.setInitialized(true);
+		}
+		return null;
 	}
 
 
